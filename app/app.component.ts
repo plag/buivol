128959f0df5f273be408b34ed53a144ce391d303
Victor Denisov <mr.plag@gmail.com>
2016-09-17 13:36:46 +0300

    edit exercise

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My Second Angular 2 App</h1>'
+  template: `<h1>{{title}}</h1><h2>"{{exercise.name}}" описание</h2>
+    <div><label>id: </label>{{exercise.id}}</div>
+    <div>
+      <label>Название: </label>
+      <input [(ngModel)]="exercise.name" placeholder="название"/>
+    </div>`
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Workout journal';
+  exercise: Exercise = {
+    id: 1,
+    name: 'Жим лежа'
+  }
+}
+
+export class Exercise {
+  id: number;
+  name: string;
+}
